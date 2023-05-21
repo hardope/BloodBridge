8a4144b558e18fbfcb0f11dc78589b2d5ab3ba04
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-05-21 22:16:08 +0000

    Updated

--- main/views.py
+++ main/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello, world.... You're at the main index.")
