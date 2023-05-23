ded666143561c2a9ee28708fde79b2aed5346bba
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-05-23 14:43:37 +0000

    Updating Authenticate

--- main/static/authenticate.js
+++ main/static/authenticate.js
@@ -27,3 +27,67 @@ function sign_in(){
         }
     });
 }
+function sign_up(){
+    var username = $("#username").val();
+    var password = $("#password").val();
+    var email = $("#email").val();
+    var confirm_password = $("#confirm_password").val();
+    $.ajax({
+        url: "/authenticate/",
+        method: "POST",
+        data: {
+            action: "sign_up",
+            username: username,
+            password: password,
+            email: email,
+            confirm_password: confirm_password
+        },
+        headers: {
+            "X-CSRFToken": csrftoken
+        },
+        success: function(data){
+            console.log(data.success);
+            if(data.success){
+                $('.blocks').hide();
+                $('#verify_otp').show();
+            }
+            else{
+                $("#register_message").html(data.message);
+            }
+        }
+    });
+}
+
+function verify_otp(){
+    var otp = $("#otp").val();
+    var username = $("#username").val();
+    var password = $("#password").val();
+    var email = $("#email").val();
+    var first_name = $("#first_name").val();
+    var last_name = $("#last_name").val();
+    $.ajax({
+        url: "/authenticate/",
+        method: "POST",
+        data: {
+            action: "verify_otp",
+            otp: otp,
+            username: username,
+            password: password,
+            email: email,
+            first_name: first_name,
+            last_name: last_name
+        },
+        headers: {
+            "X-CSRFToken": csrftoken
+        },
+        success: function(data){
+            console.log(data.success);
+            if(data.success){
+                window.location.href = "/";
+            }
+            else{
+                $("#verify_otp_message").html(data.message);
+            }
+        }
+    });
+}
--- main/views.py
+++ main/views.py
@@ -12,7 +12,9 @@ def index(request):
 
 def authenticate_view(request):
     if request.method == "POST":
+
         if request.POST.get("action") == "login":
+
             username = request.POST.get("username")
             password = request.POST.get("password")
             user = authenticate(request, username=username, password=password)
@@ -22,6 +24,7 @@ def authenticate_view(request):
             else:
                 return JsonResponse({"success": False, "message": "Invalid username or password"})
         elif request.POST.get("action") == "sign_up":
+
             username = request.POST.get("username")
             email = request.POST.get("email")
             password = request.POST.get("password")
@@ -32,18 +35,28 @@ def authenticate_view(request):
                 return JsonResponse({"success": False, "message": "Username already exists"})
             if User.objects.filter(email=email).exists():
                 return JsonResponse({"success": False, "message": "Email already exists"})
-            user = User.objects.create_user(username=username, email=email, password=password)
-            user.save()
             return JsonResponse({"success": True})
+
         elif request.POST.get("action") == "verify_otp":
+
             first_name = request.POST.get("first_name")
             last_name = request.POST.get("last_name")
+            username = request.POST.get("username")
+            email = request.POST.get("email")
+            password = request.POST.get("password")
+
             otp = request.POST.get("otp")
             if otp != "1234":
                 return JsonResponse({"success": False, "message": "Invalid OTP"})
+            user = User.objects.create_user(username=username, email=email, password=password)
+            user.save()
             user = User.objects.get(username=request.POST.get("username"))
             user.first_name = first_name
             user.last_name = last_name
             user.save()
+
+            login(request, user)
+
             return JsonResponse({"success": True})
+
     return render(request, "authenticate.html")
