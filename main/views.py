429e6c25396f15b7afc5afa297c1d5b2dae8aebb
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-05-24 06:52:50 +0000

    Completed Mai uuUthentiation

--- main/views.py
+++ main/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 from .sendmail import sendmail
 from .models import Otp
 import random
-from datetime import datetime
+from datetime import datetime, timezone
 
 # Create your views here.
 def index(request):
