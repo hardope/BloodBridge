51d8367ae2c2fe3e4ec54368e6134416c8f5152f
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-05-24 05:42:26 +0000

    Added Model for Otp

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Otp(models.Model):
+    mail = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    otp = models.CharField(max_length=7)
+    tries = models.IntegerField(default=0)
+    created_at = models.DateTimeField(default=datetime.now)
