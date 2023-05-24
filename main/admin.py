bfe4777462588b87068269dca9c1145eb7282613
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-05-24 06:07:30 +0000

    Updated views.py

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Otp
 
 # Register your models here.
+admin.site.site_header = "BloodBridge Admin"
+admin.site.site_title = "BloodBridge Admin Portal"
+admin.site.index_title = "Welcome to BloodBridge Admin Portal"
+admin.site.register(Otp)
