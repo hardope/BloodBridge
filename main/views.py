53ea868c699fa79a1be065007518e0268945bf65
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-05-24 05:43:14 +0000

    Added feature for Otp Generation and sending

--- main/views.py
+++ main/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib.auth.models import User
+from .sendmail import sendmail
+from .models import Otp
+import random
 
 # Create your views here.
 def index(request):
@@ -36,6 +39,12 @@ def authenticate_view(request):
                 return JsonResponse({"success": False, "message": "Username already exists"})
             if User.objects.filter(email=email).exists():
                 return JsonResponse({"success": False, "message": "Email already exists"})
+
+            otp = Otp.objects.create(mail=email, username=username, otp=random.randint(100000, 999999))
+            otp.save()
+
+            sendmail(email, "BloodBridge OTP", f"Hello {username},\n\nYour OTP is {otp.otp}\n\nIf You did not request this code, please ignore this email.\n\nRegards,\nBloodBridge Team")
+
             return JsonResponse({"success": True})
 
         elif request.POST.get("action") == "verify_otp":
@@ -47,14 +56,25 @@ def authenticate_view(request):
             password = request.POST.get("password")
 
             otp = request.POST.get("otp")
-            if otp != "1234":
+            try:
+                otp = Otp.objects.get(username=username, mail=email).tries += 1
+                otp.save()
+            except:
                 return JsonResponse({"success": False, "message": "Invalid OTP"})
+            if otp != Otp.objects.get(username=username, mail=email).otp:
+                return JsonResponse({"success": False, "message": "Invalid OTP"})
+            elif Otp.objects.get(username=username, mail=email).tries >= 5:
+                Otp.objects.get(username=username, mail=email).delete()
+                return JsonResponse({"success": False, "message": "Too many tries. Please try again later."})
+
             user = User.objects.create_user(username=username, email=email, password=password)
             user.save()
             user = User.objects.get(username=request.POST.get("username"))
             user.first_name = first_name
             user.last_name = last_name
             user.save()
+
+            Otp.objects.get(username=username, mail=email).delete()
 
             login(request, user)
 
