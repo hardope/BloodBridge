8b726be84bf8c81f409e0eb6250fe557f341e948
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-05-24 05:45:24 +0000

    Created sendmail Script

---
+++ main/sendmail.py
@@ -0,0 +1,24 @@
+import os
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+
+def send_mail(to, subject, body):
+    with open("password", 'r') as f:
+        password = f.read()
+    sender = "clickviralng@gmail.com"
+    recipient = to
+
+    mail = EmailMessage()
+    mail["From"] = sender
+    mail["To"] = recipient
+    mail["Subject"] = subject
+
+    mail.set_content(body)
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
+        smtp.login(sender, password)
+        smtp.sendmail(sender, recipient, mail.as_string())
