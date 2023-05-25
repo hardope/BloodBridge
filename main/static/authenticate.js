8c245fcd004c0c7f4e856a42c422f5bba64813ac
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-05-25 08:23:32 +0000

    Loading

--- main/static/authenticate.js
+++ main/static/authenticate.js
@@ -4,7 +4,7 @@ function toggle(block) {
 }
 function sign_in(){
     $('#login_message').html('Logging in...');
-    $('#login_button').html('<image src="/static/images/loading.gif" width="60px" height="20px">')
+    $('#login_button').html('<image src="/static/images/loading.gif" width="60px">')
     var username = $("#login #username").val();
     var password = $("#login #password").val();
     $.ajax({
@@ -32,7 +32,7 @@ function sign_in(){
 }
 function sign_up(){
     $('sign_up_message').html('Signing up...');
-    $('sign_up_button').html('<image src="/static/images/loading.gif" width="60px" height="20px">')
+    $('sign_up_button').html('<image src="/static/images/loading.gif" width="60px">')
     var username = $("#sign_up #username").val();
     var password = $("#sign_up #password").val();
     var email = $("#sign_up #email").val();
@@ -66,7 +66,7 @@ function sign_up(){
 
 function verify_otp(){
     $("#verify_otp_message").html('Verifying OTP...');
-    $("#verify_otp_button").html('<image src="/static/images/loading.gif" width="60px" height="20px">')
+    $("#verify_otp_button").html('<image src="/static/images/loading.gif" width="60px">')
     var otp = $("#otp").val();
     var username = $("#sign_up #username").val();
     var password = $("#sign_up #password").val();
