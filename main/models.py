328a2a490420bb29fad1f0fa6a3c52c665b7f5a4
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-05-25 08:56:19 +0000

    Updated Authentication

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Otp(models.Model):
@@ -8,3 +9,11 @@ class Otp(models.Model):
     otp = models.CharField(max_length=7)
     tries = models.IntegerField(default=0)
     created_at = models.DateTimeField(default=datetime.now)
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100, default='')
+    phone = models.CharField(max_length=10, default='')
+    city = models.CharField(max_length=100, default='')
+    state = models.CharField(max_length=100, default='')
+    donor = models.BooleanField(default=True)
