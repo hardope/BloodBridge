3032bf7a4d10e5e935cc255b506e38e8fa25d015
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-05-25 08:37:05 +0000

    Updated AUthentication

--- main/static/authenticate.js
+++ main/static/authenticate.js
@@ -30,6 +30,12 @@ function sign_in(){
         }
     });
 }
+
+function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
+
 function sign_up(){
     $('sign_up_message').html('Signing up...');
     $('sign_up_button').html('<image src="/static/images/loading.gif" width="60px">')
@@ -37,6 +43,28 @@ function sign_up(){
     var password = $("#sign_up #password").val();
     var email = $("#sign_up #email").val();
     var confirm_password = $("#sign_up #confirm_password").val();
+    if (username.length < 1){
+        $("#sign_up_message").html("Username cannot be empty");
+        $('#sign_up_button').html('Sign Up');
+    }
+    if (password != confirm_password){
+        $("#sign_up_message").html("Passwords don't match");
+        $('#sign_up_button').html('Sign Up');
+        return;
+    }
+    if (password.length < 8){
+        $("#sign_up_message").html("Password must be atleast 8 characters long");
+        $('#sign_up_button').html('Sign Up');
+        return;
+    }
+    if (email.length < 1){
+        $("#sign_up_message").html("Email cannot be empty");
+        $('#sign_up_button').html('Sign Up');
+    }
+    if (validateEmail(email) == false){
+        $("#sign_up_message").html("Invalid email");
+        $('#sign_up_button').html('Sign Up');
+    }
     $.ajax({
         url: "/authenticate/",
         method: "POST",
@@ -73,6 +101,16 @@ function verify_otp(){
     var email = $("#sign_up #email").val();
     var first_name = $("#first_name").val();
     var last_name = $("#last_name").val();
+    if (otp.length != 6){
+        $("#verify_otp_message").html("OTP must be 6 digits long");
+        $("#verify_otp_button").html('Verify OTP');
+        return;
+    }
+    if (first_name.length < 1 || last_name.length < 1){
+        $("#verify_otp_message").html("First name and last name cannot be empty");
+        $("#verify_otp_button").html('Verify OTP');
+        return;
+    }
     $.ajax({
         url: "/authenticate/",
         method: "POST",
